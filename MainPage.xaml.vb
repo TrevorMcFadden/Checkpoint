0982787660caee23ed2186f1868401a2476675a4
Trevor McFadden ✝️ <88262580+TrevorMcFadden@users.noreply.github.com>
2022-08-12 21:30:52 -0700

    Updated MainPage.xaml.vb

--- MainPage.xaml.vb
+++ MainPage.xaml.vb
@@ -1,17 +1,19 @@
 ﻿Public NotInheritable Class MainPage
     Inherits Page
-    Private Sub RefreshButton_Click(sender As Object, e As RoutedEventArgs) Handles RefreshButton.Click
-        Navi.Refresh()
-    End Sub
-    Private Sub StopButton_Click(sender As Object, e As RoutedEventArgs) Handles RefreshButton.Click
-        Navi.Stop()
-    End Sub
+    ''CheckpointCommandBar subroutines
     Private Sub BackButton_Click(sender As Object, e As RoutedEventArgs) Handles BackButton.Click
         Navi.GoBack()
     End Sub
     Private Sub ForwardButton_Click(sender As Object, e As RoutedEventArgs) Handles ForwardButton.Click
         Navi.GoForward()
     End Sub
+        Private Sub RefreshButton_Click(sender As Object, e As RoutedEventArgs) Handles RefreshButton.Click
+        Navi.Refresh()
+    End Sub
+    Private Sub StopButton_Click(sender As Object, e As RoutedEventArgs) Handles RefreshButton.Click
+        Navi.Stop()
+    End Sub
+    ''Navi subroutine
     Private Sub Navi_LoadCompleted(sender As Object, e As NavigationEventArgs) Handles Navi.LoadCompleted
         If Navi.CanGoBack = True Then
             BackButton.IsEnabled = True
